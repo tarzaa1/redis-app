b67bdbae134a48c2d86acda15564dc449e7eb85d
tarzaa <tarek.zaarour@insight-centre.org>
2023-01-03 17:17:55 +0000

    dress up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='redis-app',
+    version='0.1',
+    description='a toy app that uses walrus to interact with redis streams',
+    author='Tarek Zaarour',
+    author_email='tarek.zaarour@insight-centre.org',
+    packages=['app'],
+    zip_safe=False
+)
