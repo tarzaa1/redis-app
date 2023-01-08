f7849c0a265d84ece70ab5df75733614fea1ea06
tarzaa <tarek.zaarour@insight-centre.org>
2023-01-08 19:26:48 +0000

    default write kwargs and yield

--- app/redis_utils/streams.py
+++ app/redis_utils/streams.py
@@ -5,13 +5,23 @@ from .base import BasicStream, StreamFactory
 class RedisStreamAndConsume(BasicStream):
 
     def __init__(self, redis_db, key, max_stream_length=None, block=0, create_cg=True):
-        super().__init__(key)
+        BasicStream.__init__(self, key)
         self.block = block
         self.redis_db = redis_db
         self.create_cg = create_cg
         self.output_stream = self._get_stream(key)
         self.input_consumer_group = self._get_consumer_group(key)
         self.max_stream_length = max_stream_length
+        self.default_write_kwargs = self._get_default_write_kwargs()
+
+    def _get_default_write_kwargs(self):
+        write_kwargs = {}
+        if self.max_stream_length is not None:
+            write_kwargs.update({
+                'maxlen': self.max_stream_length,
+                'approximate': False
+            })
+        return write_kwargs
 
     def _get_consumer_group(self, key):
         cg_name = 'cg-%s' % key
@@ -23,7 +33,7 @@ class RedisStreamAndConsume(BasicStream):
 
     def write_events(self, *events):
         return [
-            self.output_stream.add(event) for event in events
+            self.output_stream.add(event, **self.default_write_kwargs) for event in events
         ]
 
     def read_events(self, count=1):
@@ -37,23 +47,35 @@ class RedisStreamAndConsume(BasicStream):
 class RedisStreamOnly(BasicStream):
 
     def __init__(self, redis_db, key, max_stream_length=None, block=0):
-        super().__init__(key)
+        BasicStream.__init__(self, key)
         self.block = block
         self.redis_db = redis_db
         self.stream = self._get_stream(key)
         self.event_id = None
+        if self.stream.length() != 0:
+            self.event_id = self.stream.info()['last-entry'][0]
         self.max_stream_length = max_stream_length
+        self.default_write_kwargs = self._get_default_write_kwargs()
+
+    def _get_default_write_kwargs(self):
+        write_kwargs = {}
+        if self.max_stream_length is not None:
+            write_kwargs.update({
+                'maxlen': self.max_stream_length,
+                'approximate': False
+            })
+        return write_kwargs
 
     def write_events(self, *events):
         return [
-            self.stream.add(event) for event in events
+            self.stream.add(event, **self.default_write_kwargs) for event in events
         ]
 
-    def read_events(self, count=3):
-        message = self.stream.read(count=count, last_id=self.event_id, block=self.block)
-        if message:
-            self.event_id = message[-1][0]
-            return message
+    def read_events(self, count=1):
+        events = self.stream.read(count=count, last_id=self.event_id, block=self.block)
+        if events:
+            self.event_id = events[-1][0]
+            yield from events
 
     def _get_stream(self, key):
         return self.redis_db.Stream(key)
